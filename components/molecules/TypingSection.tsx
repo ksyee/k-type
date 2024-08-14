326e90b2438cead285c8853304588f52333f9c77
ksyee <tjssud0647@gmail.com>
2024-08-14 16:05:25 +0900

    [docs] TypingSection.tsx 초기 문구 fetch 형식 변경 및 다크모드 스타일링
    - JSON 데이터를 받아와서 배열로 저장, 해당 배열의 인덱스를
      'Fisher-Yates 셔플 알고리즘'으로 섞음
    - 저장한 무작위 배열의 0번째 인덱스부터 순서대로 화면에 표시하도록 설정
    - 다크모드와 라이트모드의 스타일링을 다르게 마크업

--- components/molecules/TypingSection.tsx
+++ components/molecules/TypingSection.tsx
@@ -1,16 +1,16 @@
 'use client';
-import { useState, useEffect, useCallback, useRef } from 'react';
+import { useState, useEffect, useRef, useCallback, use } from 'react';
+import { useCpmTimer } from '@/hooks/useCpmTimer';
+import { useIsDarkMode } from '@/hooks/useIsDarkMode';
 import { useCpmStore } from '@/store/cpmStore';
 import { useReportStore } from '@/store/reportStore';
+import { useAccuracyStore } from '@/store/accuracyStore';
 import { Report } from '@/components/molecules';
 
 import { fetchSentence } from '@/services/api';
+import { shuffleArray } from '@/utils/shuffleArray';
 import { calculateCharColors } from '@/utils/colorUtils';
-import {
-  handleInput,
-  handleEnter,
-  keyPressEscape,
-} from '@/handlers/inputHandlers';
+import { handleInput, handleEnter, clearInput } from '@/handlers/inputHandlers';
 
 export function TypingSection() {
   const {
@@ -24,35 +24,52 @@ export function TypingSection() {
   const { startTime } = typingTime;
 
   const { report, setReport } = useReportStore();
+  const { accuracy, setAccuracy } = useAccuracyStore();
 
+  const [sentences, setSentences] = useState<
+    { speaker: string; text: string }[]
+  >([]);
   const [sentence, setSentence] = useState<{ speaker: string; text: string }>({
     speaker: 'Loading...',
     text: '문장을 불러오는 중입니다...',
   });
+  const [usedSentences, setUsedSentences] = useState<string[]>([]);
   const [displayWord, setDisplayWord] = useState<string>(sentence.text);
-  const [charColors, setCharColors] = useState<string[]>(
-    Array(displayWord.length).fill('white')
-  );
+
+  const isDarkMode = useIsDarkMode();
+  // 다크모드 여부에 따라 초기 글자 색상 설정
+  const initialCharColors = isDarkMode
+    ? Array(sentence.text.length).fill('white')
+    : Array(sentence.text.length).fill('#999999');
+  const [charColors, setCharColors] = useState<string[]>(initialCharColors);
+
   const [textareaLines, setTextareaLines] = useState<number>(1);
 
-  const usedSentencesRef = useRef<Set<string>>(new Set());
   const inputRef = useRef<HTMLTextAreaElement>(null);
 
-  // CPM 계산을 위한 interval 설정
-  const startCpmInterval = useCallback(() => {
-    const intervalId = setInterval(() => {
-      const currentCpm = calculateCpm();
-      setCpm(currentCpm);
-    }, 100);
-
-    return () => clearInterval(intervalId);
-  }, [calculateCpm, setCpm]);
+  useCpmTimer(calculateCpm, setCpm);
 
   // 화면 클릭 시 input에 포커스
   const handleScreenClick = () => {
     if (inputRef.current) {
       inputRef.current.focus();
     }
+  };
+
+  // 사용한 문장을 저장하고 새로운 문장을 가져옴
+  const fetchNewSentence = async () => {
+    if (sentences.length === 0) return;
+
+    let sentenceCount = 1;
+    let newSentence;
+
+    do {
+      newSentence = sentences[sentenceCount];
+      sentenceCount++;
+    } while (usedSentences.includes(newSentence.text));
+
+    setUsedSentences((prev) => [...prev, newSentence.text]);
+    setSentence(newSentence);
   };
 
   // 결과 저장
@@ -66,33 +83,62 @@ export function TypingSection() {
       (e.key === 'Enter' || e.key === ' ')
     ) {
       setReport({ cpm: currentCpm, accuracy, count: count + 1 });
-      fetchSentence();
+      fetchNewSentence();
+      clearInput(setInputValue, setCpm, setTime, setTextareaLines);
     }
   };
 
-  // 데이터 베이스에서 랜덤한 문장을 가져와서 화면에 표시
+  // 최초 렌더링 시 문장 데이터를 가져와 저장 후 배열을 랜덤으로 섞음
   useEffect(() => {
-    fetchSentence();
-    console.log('fetchSentence');
+    (async () => {
+      try {
+        const fetchSentencesData = await fetchSentence();
+        setSentences(shuffleArray(fetchSentencesData));
+
+        // 데이터를 받아온 후 랜덤으로 문장을 선택하여 초기화
+        const initialSentence = fetchSentencesData[0];
+        setSentence(initialSentence);
+      } catch (error) {
+        console.error('Error fetching sentence:', error);
+      }
+    })();
   }, []);
 
   // 입력한 글자와 문장을 비교하여 색상을 변경
   useEffect(() => {
     const { newCharColors, newDisplayWord } = calculateCharColors(
       sentence.text,
-      inputValue
+      inputValue,
+      isDarkMode
     );
 
     setCharColors(newCharColors);
     setDisplayWord(newDisplayWord);
-  }, [inputValue, sentence]);
+  }, [inputValue, sentence, isDarkMode]);
+
+  // 입력한 글자 처리
+  const handleInputCallback = useCallback(
+    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      handleInput(
+        e,
+        startTime,
+        setTime,
+        setInputValue,
+        () => {},
+        setCpm,
+        inputValue,
+        setTextareaLines
+      );
+    },
+    [startTime, setTime, setInputValue, setCpm, inputValue, setTextareaLines]
+  );
 
   return (
     <section className="relative h-screen w-screen" onClick={handleScreenClick}>
       <h2 className="sr-only">타이핑 섹션</h2>
-      <div className="absolute left-1/2 top-1/3 w-[80%] min-w-[800px] max-w-[900px] -translate-x-1/2">
+      <div className="absolute left-1/2 top-1/3 w-[80%] min-w-[800px] max-w-[900px] -translate-x-1/2 space-y-4pxr">
         <Report />
-        <div className="overflow-hidden rounded-lg bg-zinc-700 px-48pxr py-48pxr text-zinc-50">
+        <div className="overflow-hidden rounded bg-zinc-50 px-48pxr py-48pxr text-zinc-800 shadow-md dark:bg-zinc-700 dark:text-zinc-50">
           <span>-{sentence.speaker}-</span>
           <p className="mb-8pxr mt-8pxr text-20pxr">
             {displayWord.split('').map((char, index) => {
@@ -106,21 +152,12 @@ export function TypingSection() {
           <textarea
             ref={inputRef}
             id="typing-input"
-            className="w-full resize-none overflow-hidden border-b-4 border-zinc-400 bg-transparent text-20pxr text-zinc-50 outline-none"
-            onChange={(e) =>
-              handleInput(
-                e,
-                startTime,
-                setTime,
-                setInputValue,
-                startCpmInterval,
-                setCpm,
-                inputValue,
-                setTextareaLines
-              )
-            }
+            className="w-full resize-none overflow-hidden border-b-4 border-yellow-300 bg-transparent text-20pxr text-zinc-800 outline-none dark:border-zinc-400 dark:text-zinc-50"
+            onChange={handleInputCallback}
             onKeyDown={(e) => {
-              keyPressEscape(e, setInputValue, setCpm, setTime);
+              if (e.key === 'Escape') {
+                clearInput(setInputValue, setCpm, setTime, setTextareaLines);
+              }
               saveReport(e);
               handleEnter(e);
             }}
