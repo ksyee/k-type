6c62c4d36720a634e21fe15715333a5aa3932e98
ksyee <tjssud0647@gmail.com>
2024-08-14 16:18:12 +0900

    [docs] utils/colorUtils.ts 다크모드에 따라 다른 색상 할당

--- utils/colorUtils.ts
+++ utils/colorUtils.ts
@@ -1,27 +1,52 @@
 // 디스플레이에 표시되는 문자의 색상 계산
 export function calculateCharColors(
   sentence: string,
-  inputValue: string
+  inputValue: string,
+  isDarkMode: boolean
 ): { newCharColors: string[]; newDisplayWord: string } {
-  const newCharColors = Array(sentence.length).fill('white');
+  let newCharColors: string[] = [];
+  if (isDarkMode) {
+    newCharColors = Array(sentence.length).fill('white');
+  } else {
+    newCharColors = Array(sentence.length).fill('#999999');
+  }
   const newDisplayWord = sentence.split('');
 
   for (let i = 0; i < sentence.length; i++) {
     const inputChar = inputValue[i];
     const targetChar = sentence[i];
 
-    if (inputChar === targetChar) {
-      newCharColors[i] = '#aaaaaa';
-    }
-
-    if (i < inputValue.length - 1) {
+    // 다크 모드일 때
+    if (isDarkMode) {
       if (inputChar === targetChar) {
         newCharColors[i] = '#aaaaaa';
-      } else if (targetChar === ' ' && inputChar !== targetChar) {
-        newCharColors[i] = 'red';
-        newDisplayWord[i] = '_';
-      } else {
-        newCharColors[i] = 'red';
+      }
+
+      if (i < inputValue.length - 1) {
+        if (inputChar === targetChar) {
+          newCharColors[i] = '#aaaaaa';
+        } else if (targetChar === ' ' && inputChar !== targetChar) {
+          newCharColors[i] = 'red';
+          newDisplayWord[i] = '_';
+        } else {
+          newCharColors[i] = 'red';
+        }
+      }
+    } else {
+      // 라이트 모드일 때
+      if (inputChar === targetChar) {
+        newCharColors[i] = '#555555';
+      }
+
+      if (i < inputValue.length - 1) {
+        if (inputChar === targetChar) {
+          newCharColors[i] = '#555555';
+        } else if (targetChar === ' ' && inputChar !== targetChar) {
+          newCharColors[i] = 'red';
+          newDisplayWord[i] = '_';
+        } else {
+          newCharColors[i] = 'red';
+        }
       }
     }
   }
